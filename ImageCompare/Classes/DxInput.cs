a4d0362ba23353825f05aaa18704bc97fda2c1bb
Sebastiaan Leeflang <s.leeflang@hotmail.com>
2019-05-16 12:31:48 +0200

    test commit

--- ImageCompare/Classes/DxInput.cs
+++ ImageCompare/Classes/DxInput.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 
 namespace ImageCompare.Classes
 {
+    //test commit
     public static class DxInput
     {
         private static readonly Random rand = new Random();
